2f4ad70b4fa174dde0b378c800c952a512185667
Frode van der Meeren <frva@nordicsemi.no>
2021-05-07 10:47:33 +0200

    Added more flexibility to configuration

--- FloppyReader.py
+++ FloppyReader.py
@@ -6,32 +6,71 @@ import numpy as np
 import saleae
 import serial
 
+# Image settings
+HEADS = 2
+CYLINDERS = 83
+STARTING_CYL = 0
+TRACKSKIP = 0
+
+OUTPUT_IMAGE_FILE = "floppy2.scp"
+
+# Flags for the drive and disk used.
+flag_48tpi          = 0x00
+flag_96tpi          = 0x02
+flag_300rpm         = 0x00
+flag_360rpm         = 0x04
+
+CONFIG_FLAGS = flag_48tpi + flag_300rpm
+
+# Settings related to the analyzer setup
+ANALYZER_PRESET_FILE    = "setup8m.logicsettings"   # Name of preset-file
+SAMPLE_RATE             = 8e6                       # Bitrate sampled by the analyzer. 40e6 needs to be an integer-multiple of this.
+REVOLUTIONS             = 2                         # Revolutions per track. Sampling-time needs to be long enough to cover these.
+
+#
+# Disk-format parameters:
+#
+man_CBM         = 0x0
+man_PC          = 0x3
+man_Other       = 0x8
+
+# man_PC
+disk_PC360K     = 0x00
+disk_PC720K     = 0x01
+disk_PC12M      = 0x02
+disk_PC144M     = 0x03
+
+# man_Other
+disk_360        = 0x00
+disk_12M        = 0x01
+disk_Rrsvd1     = 0x02
+disk_Rsrvd2     = 0x03
+disk_720        = 0x04
+disk_144M       = 0x05
+
+MANUFACTURE     = man_Other
+DISK_TYPE       = disk_360
+
 class SCPWriter:
-
-    # number of tracks in a SCP files
-    __NTRACKS = 168
-
-    # number of revolutions captured per track
-    __NREVS = 2
 
     def __init__(self, imagetype):
         self.__trackdata = dict()
-        self.__trackduration = np.zeros((self.__NTRACKS, self.__NREVS))
-        self.__tracklen = np.zeros((self.__NTRACKS, self.__NREVS))
+        self.__trackduration = np.zeros((HEADS*CYLINDERS, REVOLUTIONS))
+        self.__tracklen = np.zeros((HEADS*CYLINDERS, REVOLUTIONS))
         self.__imagetype = imagetype
 
     def fileheader(self):
         # SCP header
         scp_magic = b"SCP"
-        scp_vers = 0x17 # version 1.7
+        scp_vers = 0x22 # version 2.2
         scp_type = self.__imagetype
-        scp_nrev = self.__NREVS # number of revolutions
+        scp_nrev = REVOLUTIONS # number of revolutions
         scp_starttrack = 0 # always start at track 0  not at min(self.trackdata.keys())
         scp_endtrack = max(self.__trackdata.keys())
-        scp_flags = 1 # flux data starts at index
+        scp_flags = 1+CONFIG_FLAGS # flux data starts at index
         scp_width = 0 # 16 bits
-        scp_heads = 0 # both heads
-        scp_res = 0 # 25 ns
+        scp_heads = [-1, TRACKSKIP+1, 0][HEADS]
+        scp_res = (40e6/SAMPLE_RATE) - 1
         scp_checksum = 0 # TODO
 
         scp_header = pack("<3sBBBBBBBBBL", scp_magic, scp_vers, scp_type, scp_nrev, scp_starttrack, scp_endtrack, scp_flags, scp_width, scp_heads, scp_res, scp_checksum)
@@ -39,8 +78,8 @@ class SCPWriter:
 
     def trackoffsettable(self):
         # SCP track table
-        scp_tracklist = self.__NTRACKS*[0]
-        offs = len(self.fileheader()) + self.__NTRACKS*4 # 1 long word for each track
+        scp_tracklist = HEADS*CYLINDERS*[0]
+        offs = len(self.fileheader()) + HEADS*CYLINDERS*4 # 1 long word for each track
         for k in range(len(scp_tracklist)):
             try:
                 scp_tlen = len(self.trackdata(k)) + len(self.trackheader(k)) # will raise KeyError if track does not exist
@@ -56,17 +95,17 @@ class SCPWriter:
         scp_tmagic = b"TRK"
         scp_trackno = num
         scp_trkhead = pack("<3sB", scp_tmagic, scp_trackno)
-        scp_tstart = 4 + 12*self.__NREVS # first revolution starts after this header
-        for k in range(self.__NREVS):
-            scp_tduration = round(self.__trackduration[num,k]/25e-9) 
+        scp_tstart = 4 + 12*REVOLUTIONS # first revolution starts after this header
+        for k in range(REVOLUTIONS):
+            scp_tduration = round(self.__trackduration[num,k]*SAMPLE_RATE) 
             scp_tlen = self.__tracklen[num,k] # in bitcells, not in bytes!
             scp_trkhead = scp_trkhead + pack("<LLL", int(scp_tduration), int(scp_tlen), scp_tstart)
             scp_tstart = scp_tstart + 2 * int(scp_tlen)  # start of next revolution
         return scp_trkhead
 
     def trackdata(self, num): # will raise KeyError if track does not exist
-        # round to 25 ns precision
-        bitcells = np.round(self.__trackdata[num] / 25e-9)
+        # round to sample-rate precision
+        bitcells = np.round(self.__trackdata[num]*SAMPLE_RATE)
         # pack into output data
         bitdata = pack(">%dH" % len(bitcells), *bitcells.astype('int'))
         return bitdata
@@ -77,14 +116,13 @@ class SCPWriter:
         
         # faster binary file parser: assumes index in channel 1, read_data in channel 3
         temp = iter_unpack("=qQ", open(filename,"rb").read())
-        sample_rate = 20e6
-        tr = np.rec.array([(k[0]/sample_rate,(k[1]&2) >> 1,(k[1]&8) >> 3) for k in temp], dtype=[('times','f8'),('index','i4'),('read_data','i4')])
+        tr = np.rec.array([(k[0]/SAMPLE_RATE,(k[1]&2) >> 1,(k[1]&8) >> 3) for k in temp], dtype=[('times','f8'),('index','i4'),('read_data','i4')])
 
         indexpulse = np.where(np.diff(tr.index) == -1)[0]+1 # index transitions from high->low
-        if len(indexpulse) != self.__NREVS+1:
+        if len(indexpulse) != REVOLUTIONS+1:
             print("Not enough index pulses found")
 
-        for k in range(self.__NREVS):
+        for k in range(REVOLUTIONS):
             # one revolution
             tr_rev = tr[indexpulse[k]:indexpulse[k+1]+1] # one sample overlap
             self.__trackduration[num,k] = max(tr_rev.times) - min(tr_rev.times)
@@ -120,7 +158,7 @@ class LogicAnalyzer:
     def setup(self):
         # not all setup options are exposed via the Socket API, unfortunately.
         # therefore load "canned" settings from file
-        self.s.load_from_file(os.path.join(os.getcwd(),"setup.logicsettings"))
+        self.s.load_from_file(os.path.join(os.getcwd(),ANALYZER_PRESET_FILE))
 
     def captureandsave(self, filename):
         self.s.close_all_tabs()
@@ -176,7 +214,7 @@ class FloppyDrive:
         else:
             self.p.dtr = False
 
-s = SCPWriter((1<<4) + 5)
+s = SCPWriter((MANUFACTURE<<4) + DISK_TYPE)
 l = LogicAnalyzer()
 l.setup()
 f = FloppyDrive("com3")
@@ -184,15 +222,16 @@ f.rezero()
 
 with tempfile.TemporaryDirectory() as tmpdirname:
     fname = os.path.join(tmpdirname,"export.bin")
-    for trackno in range(83):
-        print("T%dS0" % trackno)
-        f.sideselect(0)
-        l.captureandsave(fname)
-        s.loadtrack(2*trackno + 0, fname)
-        print("T%dS1" % trackno)
-        f.sideselect(1)
-        l.captureandsave(fname)
-        s.loadtrack(2*trackno + 1, fname)
+    for _ in range(STARTING_CYL):
         f.step("in")
+    for trackno in range(CYLINDERS-STARTING_CYL):
+        for headno in range(HEADS):
+            print("T%dS%d" % (trackno, headno))
+            f.sideselect(headno)
+            l.captureandsave(fname)
+            s.loadtrack(HEADS*trackno + headno, fname)
+        f.step("in")
+        for _ in range(TRACKSKIP):
+            f.step("in")
 
-s.saveimage("floppy2.scp")
+s.saveimage(OUTPUT_IMAGE_FILE)
